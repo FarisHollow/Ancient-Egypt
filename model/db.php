4880438fbdb1dc501630dfc0376a0c5923fbe5ea
FarisHollow <mirfaris79@gmail.com>
2023-09-12 18:52:35 +0600

    Error fixed

--- model/db.php
+++ model/db.php
@@ -3,30 +3,24 @@
 
 $dbhost = "localhost";
 $dbname = "egypt_db";
-$dbuser = "root";
+$dbuser = "rootqq";
 $dbpass = "";
 
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
-function getConnection(){
+function getConnection() {
 	global $dbhost;
 	global $dbname;
 	global $dbpass;
 	global $dbuser;
 	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-	
 
-
-	if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
-{
-
-	die("failed to connect!");
-}
+	if(!$conn)
+    {
+        die("failed to connect!");
+    }
 
 	return $conn;
-
-	
-
 }
 
 
